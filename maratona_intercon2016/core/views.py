cadeb99b03a271b68b7376e384db6e631bb1d2dd
patymori <excermori@yahoo.com.br>
2016-10-22 06:29:49 -0200

    Busca votos válidos por bairro

--- maratona_intercon2016/core/views.py
+++ maratona_intercon2016/core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import Bairro
+from .models import Bairro, Voto
 
 
 def index(request):
+    votos = ''
     bairros = Bairro.objects.all()
-    context = {'bairros': bairros}
+    q = request.GET.get('bairro')
+    if q:
+        votos = Voto.objects.filter(zona__bairro=q)
+    context = {'bairros': bairros, 'votos': votos}
     return render(request, 'index.html', context)
