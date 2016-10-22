d33735995b66276d9b477fb2eab33e8f55e4de63
patymori <excermori@yahoo.com.br>
2016-10-22 06:38:41 -0200

    Ordenação dos candidatos pelo total de votos

--- maratona_intercon2016/core/views.py
+++ maratona_intercon2016/core/views.py
@@ -7,6 +7,6 @@ def index(request):
     bairros = Bairro.objects.all()
     q = request.GET.get('bairro')
     if q:
-        votos = Voto.objects.filter(zona__bairro=q)
+        votos = Voto.objects.filter(zona__bairro=q).order_by('-total_votos')
     context = {'bairros': bairros, 'votos': votos}
     return render(request, 'index.html', context)
