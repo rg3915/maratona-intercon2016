7bf17cb88d31297adac5ee4302b1191965f9158f
rg3915 <rg3915@yahoo.com.br>
2016-10-22 02:42:08 -0200

    Removendo flask templates

--- freeze.py
+++
@@ -1,7 +0,0 @@
-from flask_frozen import Freezer
-from grupy_intercon2016 import app
-
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    freezer.freeze()
--- runserver.py
+++
@@ -1,6 +0,0 @@
-import os
-from grupy_intercon2016 import app
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=True)
