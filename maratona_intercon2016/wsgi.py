fef861363c181d6502731fcca0cf495332b46bdf
patymori <excermori@yahoo.com.br>
2016-10-22 05:18:00 -0200

    Correção do arquivo wsgi.py

--- maratona_intercon2016/wsgi.py
+++ maratona_intercon2016/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maratona_intercon2016.settings")
 
 application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
+
