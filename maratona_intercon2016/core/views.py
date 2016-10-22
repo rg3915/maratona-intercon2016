d60665415e2aa29dc5a4e32b5b5a93d25fe80b16
patymori <excermori@yahoo.com.br>
2016-10-22 06:51:08 -0200

    Exibição de dados dos Eleitores

--- maratona_intercon2016/core/views.py
+++ maratona_intercon2016/core/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Bairro, Voto
+from .models import Bairro, Voto, Eleitor
 
 
 def index(request):
     votos = ''
+    eleitores = ''
     bairros = Bairro.objects.all()
     q = request.GET.get('bairro')
     if q:
         votos = Voto.objects.filter(zona__bairro=q).order_by('-total_votos')
-    context = {'bairros': bairros, 'votos': votos}
+        eleitores = Eleitor.objects.filter(zona__bairro=q).order_by('-qtd_eleitores_no_perfil')
+    context = {'bairros': bairros, 'votos': votos, 'eleitores': eleitores}
     return render(request, 'index.html', context)
